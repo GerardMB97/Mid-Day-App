1b72989d9dbd8f6c3ae860773659864c9d4f319d
Gerard Mangues <gerardcoding@gmail.com>
2021-03-06 22:45:15 +0100

    Set up server using express, and basic structure - Controllers, routes and model folders.

--- midday-userend/backend/index.js
+++ midday-userend/backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(process.env.APIURL, userRouter);
 
 app.listen(port, () => debug(`Server running in port ${chalk.blue(`${port}`)}`));
