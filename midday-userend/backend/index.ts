f2277d715facd642b2813c340e8f5b15d832c0f0
Gerard Mangues <gerardcoding@gmail.com>
2021-03-07 14:33:25 +0100

    Create a build to transpilate typescript into javascript

---
+++ midday-userend/backend/index.ts
@@ -0,0 +1,13 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.listen(port, () => debug(`Server running in port ${chalk.blue(`${port}`)}`));
