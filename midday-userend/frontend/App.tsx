2e0c7f91af616c165c266cc6c0418f047bd0073d
Gerard Mangues <gerardcoding@gmail.com>
2021-03-09 16:50:03 +0100

    Add footer component and added a LandingPage that includes categoryList component and footer

--- midday-userend/frontend/App.tsx
+++ midday-userend/frontend/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoriesList from './src/components/CategoriesList';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './src/redux/store/configureStore';
 
-export default function App() {
+import LandingPage from './src/pages/LandingPage';
+
+export default function App () {
   return (
   <Provider store = {store}>
-   <CategoriesList/>
+   <LandingPage/>
   </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
