c119968c9af92511357353cee0ea5167443a993f
Gerard Mangues <gerardcoding@gmail.com>
2021-03-19 10:55:44 +0100

    Add a populate of the dishes inside the menu

--- midday-userend/backend/src/Controllers/restaurantController/index.ts
+++ midday-userend/backend/src/Controllers/restaurantController/index.ts
@@ -3,6 +3,7 @@ export {};
 const Category = require('../../Models/categoryModel');
 const Restaurant = require('../../Models/restaurantModel');
 require('../../Models/menuModel');
+require('../../Models/dishModel');
 
 const restaurantController = () => {
   const getCategories = async (req: Request, res: Response) => {
@@ -19,7 +20,19 @@ const restaurantController = () => {
     try {
       const restaurants = await Restaurant.find({})
         .populate('category')
-        .populate('menus');
+        .populate('menus')
+        .populate({
+          path: 'menus',
+          populate: { path: 'firstCourse' }
+        })
+        .populate({
+          path: 'menus',
+          populate: { path: 'secondCourse' }
+        })
+        .populate({
+          path: 'menus',
+          populate: { path: 'dessert' }
+        });
       res.json(restaurants);
     } catch (error) {
       res.status(500);
